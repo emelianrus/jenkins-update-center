30cb65651519b6331f376c8feb7cc720625460ee
ruslan <ruslan.emelianov@yahoo.com>
2022-12-08 02:47:59 +0200

    add coreVersion logic

---
+++ pkg/coreVersion/coreVersion.go
@@ -0,0 +1,65 @@
+package coreVersion
+
+// we don't have cache for coreVersion module
+
+import (
+	"errors"
+	"io"
+	"net/http"
+
+	"github.com/sirupsen/logrus"
+)
+
+// Jenkins update center root page url
+const JENKINS_UPDATE_CENTER_URL = "https://updates.jenkins.io"
+
+const URL_LOCATION = "latestCore.txt"
+
+const (
+	STABLE_URL = JENKINS_UPDATE_CENTER_URL + "/stable/" + URL_LOCATION
+	LATEST_URL = JENKINS_UPDATE_CENTER_URL + "/current/" + URL_LOCATION
+)
+
+// Get latest core version of jenkins from update center
+// https://github.com/jenkins-infra/update-center2/blob/master/site/LAYOUT.md#latest-core-file
+func GetLatestCoreVersion() (string, error) {
+	response, err := http.Get(LATEST_URL)
+	if err != nil {
+		logrus.Errorln(err)
+		return "", err
+	}
+	content, err := io.ReadAll(response.Body)
+	defer response.Body.Close()
+
+	if err != nil {
+		logrus.Errorln(err)
+		return "", err
+	}
+
+	if response.StatusCode != 200 {
+		return "", errors.New("status code is not 200")
+	}
+	return string(content), nil
+}
+
+// Get stable core version of jenkins from update center
+// https://github.com/jenkins-infra/update-center2/blob/master/site/LAYOUT.md#latest-core-file
+func GetStableCoreVersion() (string, error) {
+	response, err := http.Get(STABLE_URL)
+	if err != nil {
+		logrus.Errorln(err)
+		return "", err
+	}
+	content, err := io.ReadAll(response.Body)
+	defer response.Body.Close()
+
+	if err != nil {
+		logrus.Errorln(err)
+		return "", err
+	}
+
+	if response.StatusCode != 200 {
+		return "", errors.New("status code is not 200")
+	}
+	return string(content), nil
+}
