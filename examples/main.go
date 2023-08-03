52425edc24f9d85287090dd202a12b5044b7231c
ruslan <ruslan.emelianov@yahoo.com>
2023-08-03 12:25:24 +0300

    force package version to v1

--- examples/main.go
+++ examples/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/emelianrus/jenkins-update-center/v2/pkg/jenkinsSite"
+	"github.com/emelianrus/jenkins-update-center/pkg/jenkinsSite"
 )
 
 func main() {
