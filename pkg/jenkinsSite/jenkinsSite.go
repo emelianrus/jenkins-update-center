a8dbdbec7385e6e566dd345ccf8df664ff075955
ruslan <ruslan.emelianov@yahoo.com>
2023-08-07 12:24:36 +0300

    Add interface

--- pkg/jenkinsSite/jenkinsSite.go
+++ pkg/jenkinsSite/jenkinsSite.go
@@ -1,11 +1,19 @@
 package jenkinsSite
 
+const JENKINS_UPDATE_CENTER_URL = "https://updates.jenkins.io"
+
+// jenkinsSiteInterface represents the methods used from the external JenkinsSite package.
+type JenkinsSiteInterface interface {
+	GetPluginVersions() (*PluginVersions, error)
+	GetUpdateCenter(coreVersion string) (*UpdateCenter, error)
+	GetStableCoreVersion() (string, error)
+	GetLatestCoreVersion() (string, error)
+}
+
 type JenkinsSite struct {
 	PluginVersions PluginVersions
 	UpdateCenter   UpdateCenter
 }
-
-const JENKINS_UPDATE_CENTER_URL = "https://updates.jenkins.io"
 
 func NewJenkinsSite() JenkinsSite {
 	pluginVersions := NewPluginVersions()
@@ -15,4 +23,12 @@ func NewJenkinsSite() JenkinsSite {
 		PluginVersions: pluginVersions,
 		UpdateCenter:   updateCenter,
 	}
+}
+
+func (js JenkinsSite) GetPluginVersions() (*PluginVersions, error) {
+	return js.PluginVersions.Get()
+}
+
+func (js JenkinsSite) GetUpdateCenter(coreVersion string) (*UpdateCenter, error) {
+	return js.UpdateCenter.Get(coreVersion)
 }
